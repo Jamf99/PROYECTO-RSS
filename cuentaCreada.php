343ee0ceba587103f16f5221e460f820daf8d5c7
Jorge Antonio Morales Florez <jamf_99@hotmail.com>
2019-06-20 11:20:48 -0500

    proyecto resubido

---
+++ cuentaCreada.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Usuario Registrado</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+
+  </head>
+  <body>
+    <div class="cntCreada">
+		     <h1> Su cuenta ha sido creada satisfactoriamente</h1>
+	</div>
+  <a href="index.php"> Iniciar Sesión</a>
+  </body>
+</html>
