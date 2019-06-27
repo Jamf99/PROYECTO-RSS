c0239b9e85b8040f96b4863b98dc92b2117d32a1
Jorge Antonio Morales Florez <jamf_99@hotmail.com>
2019-06-27 08:03:36 -0500

    Reestructuración

--- database.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$database = "proyectorss";
-$username = "root";
-$password = "";
-//crea Conexion - valida conexion
-  $conexion=mysqli_connect($servername,$username,$password,$database) or die("--Falló la conexión".mysqli_error($conexion));
-?>
