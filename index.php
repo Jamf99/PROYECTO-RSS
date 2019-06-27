57652ca0bb4048dbedf6d7630700da2cc996b264
Jorge Antonio Morales Florez <jamf_99@hotmail.com>
2019-06-27 16:14:39 -0500

    Autenticación de usuario en index

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
-<!DOCTYPE html>
+<?php
+
+  session_start();
+  require 'database.php';
+
+  if(isset($_SESSION['user_id'])) {
+    $records = $conn->prepare('SELECT id, name, email, password FROM users WHERE id = :id');
+    $records->bindParam(':id', $_SESSION['user_id']);
+    $records->execute();
+    $results = $records->fetch(PDO::FETCH_ASSOC);
+
+    $user = null;
+    
+    if (count($results) > 0) {
+      $user = $results;
+    }
+  }
+
+?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
@@ -6,10 +24,15 @@
     <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
-    <div class="contenedora">
-      <img src="assets/css/images/logo_principal.png" alt="" width="250px"><br>
-      <a class="login" href="login.php">Login</a>
-      <a class="register" href="signup.php">Register</a>
-    </div>
+    <?php if(!empty($user)): ?>
+      <h1>Welcome <?= $user['name'] ?> !</h1>
+      <br><a href="logout.php">Cerrar Sesión</a>
+    <?php else: ?>
+      <div class="contenedora">
+        <img src="assets/css/images/logo_principal.png" alt="" width="250px"><br>
+        <a class="login" href="login.php">Login</a>
+        <a class="register" href="signup.php">Register</a>
+      </div>
+    <?php endif; ?>
   </body>
 </html>
