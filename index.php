6a3d56c611b6cf95c8a204e3fd1a3972df513604
Jorge Antonio Morales Florez <jamf_99@hotmail.com>
2019-06-29 12:56:38 -0500

    Prototipo de prueba para RSS

--- index.php
+++ index.php
@@ -25,6 +25,7 @@
   </head>
   <body>
     <?php if(!empty($user)): ?>
+
       <nav>
         <div class="logo">
           <a href="index.php"><img src="assets/images/logo_principal.png" alt="" width="70px"></a><br>
@@ -34,6 +35,48 @@
           <li><a href="logout.php">Cerrar sesión</a></li>
         </ul>
       </nav>
+
+      <div class="principal">
+        <div class="suscripcion">
+          <form class="" action="index.html" method="post">
+            <label for="">Escribe el link del sitio web en dónde te quieras suscribir</label><br>
+            <input type="text" name="" value=""><br>
+            <input type="submit" name="" value="Suscribirse">
+          </form>
+        </div>
+
+        <div class="noticias">
+          <?php
+
+            $xml ='http://www.eltiempo.com/contenido/opinion/rss.xml';
+            $num_noticias = 5;
+            $long_description = 100;
+            $noticias = simplexml_load_file($xml);
+
+            foreach($noticias as $noticia) {
+              $n = 0;
+              foreach($noticia as $reg){
+                if($reg->title != NULL && $reg->title != '' && $reg->description != NULL && $reg->description != '' && $n < $num_noticias) {
+                  ?> <div class="noticia"> <?php
+                    echo '<h4><a href="'.$reg->link.'" target="_blank">'.$reg->title.'</a></h4>';
+                    if(strlen($reg->description) > $long_description){
+                      echo '<p>'.substr($reg->description, 0, $long_description).'....</a></p>';
+                    }else if($reg->description == NULL || $reg->description == '') {
+
+                    }else {
+
+                    }
+                    $n++;
+                  ?></div> <?php
+                }
+              }
+            }
+            ?>
+        </div>
+      </div>
+
+
+
     <?php else: ?>
       <div class="contenedora">
         <img src="assets/images/logo_principal.png" alt="" width="250px"><br>
